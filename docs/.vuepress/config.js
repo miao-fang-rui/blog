f58ec3d665f92a556d3f9a42a8345c42878fce35
miao-fang-rui <miaofangrui@163.com>
2024-09-21 13:59:29 +0800

    配置工作流

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,4 +5,20 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
     bundler: viteBundler(),
     theme: defaultTheme(),
+    base: '/blog/',
+
+    locales: {
+        '/': {
+            lang: 'en-US',
+            title: 'Tiamaes',
+            description: 'Tiamaes Document center',
+            selectLanguageName: 'English',
+        },
+        '/zh/': {
+            lang: 'zh-CN',
+            title: '天迈科技',
+            description: '天迈文档中心',
+            selectLanguageName: '简体中文',
+        },
+    },
 })
