a8bd803671207cde239a3328be9441d732bb7fd3
miao-fang-rui <miaofangrui@163.com>
2025-02-10 08:56:12 +0800

    关闭figure

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,13 +7,10 @@ import { mdEnhancePlugin } from "vuepress-plugin-md-enhance";
 export default defineUserConfig({
     bundler: viteBundler(),
     base: '/blog/',
-    head: [
-        ['link', { rel: 'icon', href: 'favicon.ico' }],
-    ],
     plugins: [
         markdownImagePlugin({
             // 启用 figure
-            figure: true,
+            // figure: true,
             // 启用图片懒加载
             lazyload: true,
             // 启用图片标记
@@ -50,12 +47,18 @@ export default defineUserConfig({
             title: 'Tiamaes',
             description: 'Tiamaes Document center',
             selectLanguageName: 'English',
+            head: [
+                ['link', { rel: 'icon', href: 'favicon.ico' }],
+            ],
         },
         '/zh/': {
             lang: 'zh-CN',
             title: '天迈科技',
             description: '天迈文档中心',
             selectLanguageName: '简体中文',
+            head: [
+                ['link', { rel: 'icon', href: 'favicon.ico' }],
+            ],
         },
     },
     theme: defaultTheme({
@@ -121,13 +124,16 @@ export default defineUserConfig({
                 selectLanguageText: '选择语言',
                 toggleColorMode: '白天 / 黑夜',
                 notFound: [
-                    '生活不易，我也叹气，要不刷新一下再继续？',
-                    '如果我能够看到自己的内容，我想它一定充满忧伤，因为我把快乐都给了其他页面。',
-                    '请给我点时间，我会想开的！',
-                    '本想走到你心里，一不小心迷路了。',
-                    '从前有座山，山里有座庙， 庙里有个页面，现在找不到了. . .',
-                    '大师兄，页面被妖怪抓走了. . .',
-                    '我生病了~正在诊断哦~乖乖等我!',
+                    '生活或许充满挑战，但请相信，每一次跌倒都是为了更好地站起，每一次泪水都是成长的洗礼。',
+                    '特别生气的时候也不要跟亲人、爱人说那些恶毒的话，说过的话是收不回的。',
+                    '生活就像坐过山车，有高峰，也有低谷，这意味着，无论眼下是好是坏，都只是暂时的。',
+                    '自由不是想干什么就干什么，而是不想干什么，就有能力不干什么。',
+                    '无论正在经历什么，都请不要轻言放弃，因为从来没有一种坚持会被辜负。',
+                    '与其嫉妒别人，不如用实际行动超越别人。',
+                    '去见你想见的人，去做你想做的事，趁阳光正好，趁微风不燥，趁繁华还未开至荼靡，趁我们都还年轻。',
+                    '在人生的舞台上，我们都是自己故事的主角。有时欢笑如歌，有时泪水涟涟，但正是这些情感的交织，构成了丰富多彩的人生画卷。',
+                    '每天多一点点的努力，不为别的，只为了日后能够多一些选择，选择云卷云舒的小日子，选择自己喜欢的人。',
+                    '若美好迟迟未发生也不要着急，最好的总会在不经意间出现，你的幸福只是姗姗来迟。而你只需努力，剩下的交给时光。'
                 ],
                 backToHome: '返回首页',
                 prev: '返回',
