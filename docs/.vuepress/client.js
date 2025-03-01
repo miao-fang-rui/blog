c406086e66e43e01c8fa812d65b0a39e35163175
miao-fang-rui <miaofangrui@163.com>
2025-03-01 14:06:41 +0800

    恢复

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -17,12 +17,6 @@ import Loading from './components/Loading.vue'
 export default defineClientConfig({
     enhance({ app, router, siteData }) {
 
-        router.addRoute({
-            path: '/',
-            redirect: '/login.html'
-        })
-
-        // app.component("Login", Login)
         app.component('Products', Products)
         app.component('Article', Article)
         app.component('Table', Table)
